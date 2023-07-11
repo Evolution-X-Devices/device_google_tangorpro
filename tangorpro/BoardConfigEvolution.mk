43877ab74c2883e0a1362986cff9ecde9a2546c4
AnierinB <anierin@evolution-x.org>
2023-07-10 17:36:45 -0700

    tangorpro: Initialize Evolution X 8.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- tangorpro/BoardConfigEvolution.mk
+++ tangorpro/BoardConfigEvolution.mk
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-include device/google/tangorpro/BoardConfigLineage.mk
+include device/google/tangorpro/BoardConfigEvolution.mk
 
 include vendor/google/tangorpro/BoardConfigVendor.mk
